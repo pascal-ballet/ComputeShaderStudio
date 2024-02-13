f7d09264c7647951a68a7fbb48458f5b33bcba3f
Pascal Ballet <pascal.ballet@gmail.com>
2024-02-13 15:22:45 +0100

    Update example_5.cpp

--- examples/example_5.cpp
+++ examples/example_5.cpp
@@ -3,5 +3,9 @@ void main() {
 	uint x = gl_GlobalInvocationID.x;
 	uint y = gl_GlobalInvocationID.y;
 	uint p = x + y * WSX;
-	data_0[p] = 0xFFFF0000 - int(x)*(step) + int(y);
+	int dx = 64 - int(x);
+	int dy = 64 - int(y);
+	int r = int(64.0*sin(step/100.0));
+	int d = int(dx*dx+dy*dy) ;
+	data_0[p] = 0xFF000000 - 1500* (d + step) ;
 }
