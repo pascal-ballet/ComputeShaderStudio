e0796c01e6794e32fba958c71a2d5a1d63b72da2
axel0302 <lefaucheuraxel@gmail.com>
2025-03-10 16:32:01 +0000

    annulation

--- examples/exemple_axel_mathéo/exemple.cpp
+++
@@ -1,8 +0,0 @@
-void main() {
-    uint x = gl_GlobalInvocationID.x;
-    uint y = gl_GlobalInvocationID.y;
-    uint p = x + y * WSX;
-    
-    // Logique de calcul du shader
-    data_0[p] = 0xFF000000;  // Exemple : pixel noir
-}
