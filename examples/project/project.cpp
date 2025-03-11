1646b835cc42f2a67b66f2ebb1fd7087f7334d53
redalhdd <redalahdoudi@gmail.com>
2025-03-11 12:02:46 +0100

    Presque sharingan

--- examples/project/project.cpp
+++ examples/project/project.cpp
@@ -35,16 +35,29 @@ void main() {
     float borderWidth = 2.0;
     float borderRadius = innerRadius + 15.0; // Un peu plus grand que le cercle intérieur
     
-    // Moving circle parameters
+    // Moving circle parameters (3 circles)
     float movingCircleRadius = 5.0;
+    // Premier cercle (position originale)
     float movingCircleX = centerX + borderRadius * cos(angle);
     float movingCircleY = centerY + borderRadius * sin(angle);
     float movingCircleDistance = sqrt(pow(float(x) - movingCircleX, 2.0) + pow(float(y) - movingCircleY, 2.0));
+    
+    // Deuxième cercle (décalé de 120 degrés)
+    float movingCircle2X = centerX + borderRadius * cos(angle + 2.0944); // 2.0944 = 2π/3
+    float movingCircle2Y = centerY + borderRadius * sin(angle + 2.0944);
+    float movingCircle2Distance = sqrt(pow(float(x) - movingCircle2X, 2.0) + pow(float(y) - movingCircle2Y, 2.0));
+    
+    // Troisième cercle (décalé de 240 degrés)
+    float movingCircle3X = centerX + borderRadius * cos(angle + 4.1888); // 4.1888 = 4π/3
+    float movingCircle3Y = centerY + borderRadius * sin(angle + 4.1888);
+    float movingCircle3Distance = sqrt(pow(float(x) - movingCircle3X, 2.0) + pow(float(y) - movingCircle3Y, 2.0));
 
     // Draw circle with line and inner circle
     if (distance < radius) {
-        if (movingCircleDistance < movingCircleRadius) {
-            data_0[p] = 0xFF000000; // Black moving circle
+        if (movingCircleDistance < movingCircleRadius || 
+            movingCircle2Distance < movingCircleRadius ||
+            movingCircle3Distance < movingCircleRadius) {
+            data_0[p] = 0xFF000000; // Black moving circles
         } else if (distance < innerRadius) {
             data_0[p] = 0xFF000000; // Black inner circle
         } else if (distance < borderRadius && distance > borderRadius - borderWidth) {
