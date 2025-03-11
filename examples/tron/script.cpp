269f44fc9ed6110d3cf6cda00b0ac003c0f23802
zPrisim <76958083+zPrisim@users.noreply.github.com>
2025-03-11 08:19:05 +0100

    fix pre-projet

--- examples/tron/script.cpp
+++ examples/tron/script.cpp
@@ -1,13 +1,6 @@
 #define MORT 0xFFFFFFFF
 #define VIVANT 0xFF000000
 
-// shared uint incX;
-
-/*layout(set = 0, binding = 0) buffer incBuffer
-{
-    uint incX;
-    uint incY;
-};*/
 
 struct color
 {
@@ -38,7 +31,7 @@ void dessinerSphere(uint centerX, uint centerY, float radius, color c)
     // Si le pixel est a l'interieur du cercle
     if (distanceCarre <= radius * radius)
     {
-        data_0[p] = rgb_to_int(c); // ROUGE
+        data_0[p] = rgb_to_int(c) + step; // ROUGE
     }
 }
 
@@ -55,43 +48,18 @@ void main()
     color c2 = {255, 25, 87};  // bgra
     color c3 = {30, 0, 16};    // bgra
 
-    /*
-    if (rayon + step + rayon < 800)
-    {
-        dessinerSphere(rayon + incX, rayon, rayon, c);
-        incX++;
-    }
-*/
+    if(step == 0)
+        data_0[p] = MORT;
 
-    // data_0[p] = MORT;
 
-    // dessinerSphere( uint(mousex), uint(mousey) , rayon, c1);
-
-    data_0[p] = MORT;
-
+/*
     if ((mousex >= 0) || (mousey >= 0))
     {
         dessinerSphere(mousex, mousey, rayon, c1);
     }
-
-    /*
-    if(pos.x < 400 && pos.y < 225)
-    {
-        dessinerSphere(  incX*2, incX , incX, c);
-    }
-    else
-    {
-        dessinerSphere(  incX*2, incX , 40 , c);
-    }
-
-    for(uint i = 0; i < 800; i+=100)
-    {
-        for (uint j = 0; j < 450; j+=50)
-        {
-            dessinerSphere( i , j , rayon, c1);
-        }
-    }
-    */
+*/
+    if(left_pressed)
+        dessinerSphere(mousex, mousey, rayon, c1);
     /*
     dessinerSphere( 150 , 50 , rayon, c1);
     dessinerSphere( 250 , 50 , rayon, c2);
@@ -104,19 +72,5 @@ void main()
     dessinerSphere( 100 ,   incX , rayon*2, c1);
     dessinerSphere( 300 ,   200 , rayon*2, c2);
     */
-    // incX++;
 
-    /*else
-    {
-        dessinerSphere(rayon + incX, rayon, rayon, c);
-        incX--;
-    }
-    */
-    /*
-     if (incY + rayon < 450 )
-     {
-         dessinerSphere(rayon, rayon + incY, rayon, c);
-         incY++;
-     }*/
-    // Dessiner la sphere pour chaque pixel
 }
