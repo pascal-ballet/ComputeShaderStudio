8d8be74ad6dedf54a8136404fde824c0f49068cf
radia0203 <lemraniradia@gmail.com>
2025-03-11 11:55:27 +0100

    essai icon

--- examples/heart/heart.cpp
+++ examples/heart/heart.cpp
@@ -1,2 +1,26 @@
-// c'est un test de git
-//test de git 
+
+// Write your code HERE
+void main() {
+	uint x = gl_GlobalInvocationID.x;
+	uint y = gl_GlobalInvocationID.y;
+	uint p = x + y * WSX;
+	data_0[p] = 0xFFF00FFF - int(p)*(step+1);
+}
+
+
+
+
+
+
+
+/*
+
+
+
+
+
+
+
+
+
+*/
