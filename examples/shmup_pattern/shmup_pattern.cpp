0ca45302418ec52e886d1b803aa8ccfd69eeb7c5
Ewan Durand <ewan-dur-23@hotmail.fr>
2025-03-13 10:05:39 +0100

    Génération de pattern
    Test : implémentation de pseudo aléatoire pour faire varier la vitesse

--- examples/shmup_pattern/shmup_pattern.cpp
+++ examples/shmup_pattern/shmup_pattern.cpp
@@ -3,13 +3,38 @@
 #define BLUE 0xFF0000FF
 #define BLACK 0xFF000000
 
+// Generation de bullets
 #define PI 3.1415926
 #define TWO_PI 2*PI
 #define SPAWN vec2(127,63)
-#define RAD 3
-#define NB_SPWN 8
-#define OFFSET_ANGLE PI/NB_SPWN
-#define ROTATION PI/12
+#define RAD 5
+#define NB_SPWN 12
+#define OFFSET_ROTATION PI/20
+#define SPEED 2.0
+
+// Aleatoire pour le deplacement des bullets
+#define PHI 1.61803398874989484820459
+#define RND_MOVE 0.5
+
+float compute_seed()
+{
+	float seed = 0.0;
+	int mult = 10;
+	int tmp = step;
+	while(true)
+	{
+		if(tmp == 0) break;
+		seed += (tmp%10)/mult;
+		tmp = tmp/10;
+		mult *= 10;
+	}
+	return seed;
+}
+
+float gold_noise(vec2 xy, float seed)
+{
+  return fract(tan(distance(xy*PHI, xy)*seed)*xy.x);
+}
 
 /* Layers :
  * data_0 : render
@@ -27,12 +52,12 @@ void init(uint p)
 	data_3[p] = 0;
 }
 
-vec4 compute_spawn_position_angle(int nb, int step)
+vec4 compute_spawn_position_angle(int nb, int r)
 {
 	vec4 position_direction;
 
-	position_direction.x = SPAWN.x + cos(nb * (TWO_PI/NB_SPWN) + OFFSET_ANGLE + step*ROTATION) * RAD;
-	position_direction.y = SPAWN.y + sin(nb * (TWO_PI/NB_SPWN) + OFFSET_ANGLE + step*ROTATION) * RAD;
+	position_direction.x = SPAWN.x + cos(nb * (TWO_PI/NB_SPWN) + r*OFFSET_ROTATION) * RAD;
+	position_direction.y = SPAWN.y + sin(nb * (TWO_PI/NB_SPWN) + r*OFFSET_ROTATION) * RAD;
 
 	position_direction.z = (position_direction.x - SPAWN.x)*1000;
 	position_direction.w = (position_direction.y - SPAWN.y)*1000;
@@ -53,38 +78,39 @@ void spawn_bullet(uint x, uint y, uint p, vec4 position_direction)
 void compute_movement(uint x, uint y, uint p)
 {
 	if(data_1[p] == 1){
-		// New position 
-		// Current position + movement based on vec2
-		vec2 dir = vec2(float(data_2[p])/1000, float(data_3[p])/1000);
-		float speed = 2.0;
-		float angle = atan(dir.y, dir.x);
-		float bx = float(x) + round(cos(angle)*speed);
-		float by = float(y) + round(sin(angle)*speed);
+		float seed = fract(compute_seed());
+		if(gold_noise(vec2(int(x), int(y)), seed) <= RND_MOVE){
+			// New position 
+			// Current position + movement based on vec2
+			vec2 dir = vec2(float(data_2[p])/1000, float(data_3[p])/1000);
 
-		uint dp = uint(bx) + uint(by)*WSX;
-		if((bx>0 && bx<WSX) && (by>0 && by<WSY)){
-			data_1[p] = 0;
-			data_1[dp] = 1;
+			float angle = atan(dir.y, dir.x);
+			float bx = float(x) + round(cos(angle)*SPEED);
+			float by = float(y) + round(sin(angle)*SPEED);
 
-			data_2[dp] = data_2[p];
-			data_2[p] = 0;
+			uint dp = uint(bx) + uint(by)*WSX;
+			if((bx>0 && bx<WSX) && (by>0 && by<WSY)){
+				data_1[p] = 0;
+				data_1[dp] = 1;
 
-			data_3[dp] = data_3[p];
-			data_3[p] = 0;
+				data_2[dp] = data_2[p];
+				data_2[p] = 0;
+
+				data_3[dp] = data_3[p];
+				data_3[p] = 0;
+			}
+			else{
+				data_1[p] = 0;
+				data_2[p] = 0;
+				data_3[p] = 0;
+			}
 		}
-		else{
-			data_1[p] = 0;
-			data_2[p] = 0;
-			data_3[p] = 0;
-		}
-
 	}
 }
 
-
 void render(uint x, uint y, uint p)
 {
-	if(data_1[p] == 1)
+	if(data_1[p] == 1 || data_1[p] == -1)
 		data_0[p] = GREEN;
 }
 
@@ -100,13 +126,10 @@ void main()
 	if(step == 0){
 		// Initialisation du point d'apparition des bullets
 		init(p);
-	}
-	else{
-		if(step%10 == 0){
-			for(int i=0; i<NB_SPWN; i++)
-				spawn_bullet(x,y,p,compute_spawn_position_angle(i,step/10));
-		}
 		
+	}
+	else{		
+		spawn_bullet(x,y,p,compute_spawn_position_angle(step%NB_SPWN, 1));
 		// Calcul du deplacement puis affichage du rendu
 		if(step%2 == 0)
 			compute_movement(x,y,p);
