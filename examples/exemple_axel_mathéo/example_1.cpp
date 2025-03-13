d224b680b60aced5ea46e4416e8c98169d3952a2
axel0302 <lefaucheuraxel@gmail.com>
2025-03-13 07:30:40 +0000

    Initialisation

---
+++ examples/exemple_axel_mathéo/example_1.cpp
@@ -0,0 +1,7 @@
+// Write your code HERE
+void main() {
+	uint x = gl_GlobalInvocationID.x;
+	uint y = gl_GlobalInvocationID.y;
+	uint p = x + y * WSX;
+	data_0[p] = 0xFFFF0000; //	- int(x+y*1024)*(step);
+}
