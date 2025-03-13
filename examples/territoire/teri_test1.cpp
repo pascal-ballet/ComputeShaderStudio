7c2bab41cdb78c8133f37953ca2426f27d4dadfb
Mathieu-Da-Costa <mathieu.dacosta@etudiant.univ-brest.fr>
2025-03-13 08:49:41 +0100

    Update teri_test1.cpp

--- examples/territoire/teri_test1.cpp
+++ examples/territoire/teri_test1.cpp
@@ -1,43 +1,87 @@
+#define MORT 0
+#define VIVANT 0xFFFFFFFF
+#define Rouge 0xFF0000FF  
+#define Vert 0xFF00FF00    
+#define Bleu 0xFFFF0000    
+#define Jaune 0xFFFFFF00  
 
-#define ALIVE 0xFFFFFFFF
-#define DEAD 0xFF0000FF
 
+#define MIN_VOISINS_SURVIE 1
+#define MAX_VOISINS_SURVIE 2
+#define VOISINS_NAISSANCE 1
+
+   
 void main() {
-	uint x = gl_GlobalInvocationID.x;
-	uint y = gl_GlobalInvocationID.y;
-	uint p = x + y * WSX;
-	if ( step == 0 ) { // initialisation ************************
-		if ( current_pass == 0 ) {
-			data_1[p] = DEAD ;
-			if (data_0[p] < 0 || x==0 || y==0 || x==WSX-1 || y==WSY-1)
-				data_0[p] = DEAD ;
-			else
-				data_0[p] = ALIVE ;
-		}
-	} else { // in process *********************************
-		if (current_pass == 0)
-			compute_next_step(x, y, p);
-		else
-			data_0[p] = data_1[p]; // The future is now
-            sleep(1);
-	}
-}
+   uint x = gl_GlobalInvocationID.x;
+   uint y = gl_GlobalInvocationID.y;
+   uint p = x + y * WSX;
 
-void compute_next_step(uint x, uint y, uint p) {
-		if ( x > 0 && y > 0 && x < WSX-1 && y < WSY - 1) { // in sandbox
-			uint n = 0 ; // Number of living neighbors
-			for (uint i = x-1; i <= x+1; i++) {
-				for (uint j = y-1; j <= y+1; j++) {
-					uint k = i + j * WSX;
-					if (k != p && data_0[k] == ALIVE )
-						n++;
-				}
-			}
-			if (data_0[p] == DEAD) {
-				if ( n >= 1)
-					data_1[p] = ALIVE; // Birth
-				else
-					data_1[p] = DEAD ;
-			} 
-		}
+   if (step == 0) {
+       if (data_0[p] < 0) {
+           if (x == WSX / 2 && y == WSY / 2) {  // On place une cellule vivante au centre
+               data_0[p] = VIVANT;
+           } else if (x == 1 && y== 1){
+			data_0[p] = Rouge;
+		   }else if(x == WSX-1 && y== 1){
+			data_0[p] = Vert;
+		   }else if(x == 1 && y== WSY-1){
+			data_0[p] = Bleu;
+		   }else if(x == WSX-1 && y== WSY-1){
+			data_0[p] = Jaune;
+		   } else  {
+               data_0[p] = MORT;
+           }
+       } else {
+           data_0[p] = MORT;
+       }
+   } else {
+       int nb_voisins_V = 0;
+	   int nb_voisins_R = 0;
+	   int nb_voisins_J = 0;
+	   int nb_voisins_B = 0;
+	   int nb_voisins_G = 0;
+
+       // Comptage des voisins vivants
+       for (int dy = -1; dy <= 1; dy++) {
+           for (int dx = -1; dx <= 1; dx++) {
+               if (!(dx == 0 && dy == 0)) {
+                   int nx = int(x) + dx;
+                   int ny = int(y) + dy;
+
+                   if (nx >= 0 && nx < WSX && ny >= 0 && ny < WSY) {
+                       if (data_0[uint(nx + ny * WSX)] == VIVANT) {
+                           nb_voisins_V++;
+                       }
+					   if (data_0[uint(nx + ny * WSX)] == Rouge) {
+						nb_voisins_R++;
+						}
+						if (data_0[uint(nx + ny * WSX)] == Vert) {
+							nb_voisins_G++;
+						}
+						if (data_0[uint(nx + ny * WSX)] == Bleu) {
+							nb_voisins_B++;
+						}
+						if (data_0[uint(nx + ny * WSX)] == Jaune) {
+							nb_voisins_J++;
+						}
+                   }
+               }
+           }
+       }
+
+       // La cellule morte devient vivante si elle a 1 voisin vivant
+       if (data_0[p] == MORT) {
+           if (nb_voisins_V >= 1) {
+               data_0[p] = VIVANT;
+           } else if(nb_voisins_R >= 1){
+			data_0[p] = Rouge;
+		   }else if(nb_voisins_J >= 1){
+			data_0[p] = Jaune;
+		   }else if(nb_voisins_G >= 1){
+			data_0[p] = Vert;
+		   }else if(nb_voisins_B >= 1){
+			data_0[p] = Bleu;
+		   }
+       }
+   }
 }
