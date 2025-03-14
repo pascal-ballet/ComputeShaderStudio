8df8bdecdc2ccc2606fa53aea7bcb3ad44a3692a
Pascal Ballet <pascal.ballet@gmail.com>
2025-03-14 23:56:15 +0100

    board update

--- examples/board/board.cpp
+++ examples/board/board.cpp
@@ -4,8 +4,8 @@ void main() {
     uint y = gl_GlobalInvocationID.y;
     uint p = x + y*WSX;
 
-	int case_x = int(floor(x / 16.0f));
-	int case_y = int(floor(y / 16.0f));
+	int case_x = int(floor( (x-mousex+WSX) / (150.0f*atan(3.14f*float(x+40.0f*sin(float(step)/10.0f))/float(WSX) - 3.14f ))));
+	int case_y = int(floor( (y-mousey+WSY) / (150.0f*atan(3.14f*float(y+30.0f*cos(float(step)/10.0f))/float(WSY) - 3.14f ))));
 
     if ( (case_x + case_y)%2 == 0) // Case Blanche
         data_0[p] = int(0xFFFF00FF) ; // Fond
