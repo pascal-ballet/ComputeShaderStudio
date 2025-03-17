5f819434dced031c8b869bce715e1eead81d2518
Ewan Durand <ewan-dur-23@hotmail.fr>
2025-03-17 11:48:07 +0100

    Ajout des contrôles
    - 2 patterns en simultané
    - variation des paramètres individuels avec les clicks souris

--- examples/shmup_pattern/shmup_pattern.cpp
+++ examples/shmup_pattern/shmup_pattern.cpp
@@ -1,29 +1,93 @@
 #define RED 0xFF0000FF
+#define PINK 0xFFFE25D5
 #define GREEN 0xFF00FF00
 #define BLUE 0xFFFF0000
+#define CYAN 0xFFF8F901
 #define BLACK 0xFF000000
+#define GREY 0xFF2c2c2c
 
 // Generation de bullets
 #define PI 3.1415926
 #define TWO_PI 2*PI
 #define SPAWN1 vec2(63,31)
-#define SPAWN2 vec2(191,31)
-#define RAD1 50
-#define RAD2 5
-#define NB_SPWN1 10
-#define NB_SPWN2 3
-#define OFFSET_ROTATION PI/4
+#define SPAWN2 vec2(191,63)
 #define SPEED 2.0
 
 // Aleatoire pour le deplacement des bullets
-#define RND_MOVE 0.9
+#define RND_MOVE 1.0
 
 /* Layers :
  * data_0 : render
  * data_1 : position
  * data_2 : vector.x for movement (*1000)
  * data_3 : vector.y for movement (*1000)
+ * data_4 : parameters for pattern management
  */
+
+
+void init_pattern_parameters()
+{
+	// Pattern manager 
+	data_4[0] = 0;
+	data_4[1] = 4;
+	data_4[2] = 0;
+
+	// Initial parameter values for patterns
+	// Pattern1
+	data_4[0+WSX] = 0;
+	data_4[1+WSX] = 0;
+	data_4[2+WSX] = 0;
+	data_4[3+WSX] = 0;
+	// Pattern2
+	data_4[4+WSX] = 0;
+	data_4[5+WSX] = 0;
+	data_4[6+WSX] = 0;
+	data_4[7+WSX] = 0;
+
+	// Parameters values
+	// NB_SPAWN
+	data_4[0+WSX*2] = 0;
+	data_4[1+WSX*2] = 1;
+	data_4[2+WSX*2] = 2;
+	data_4[3+WSX*2] = 3;
+	data_4[4+WSX*2] = 4;
+	data_4[5+WSX*2] = 5;
+	data_4[6+WSX*2] = 6;
+	data_4[7+WSX*2] = 8;
+	data_4[8+WSX*2] = 12;
+
+	// RADIUS
+	data_4[0+WSX*3] = 5;
+	data_4[1+WSX*3] = 10;
+	data_4[2+WSX*3] = 20;
+	data_4[3+WSX*3] = 30;
+	data_4[4+WSX*3] = 50;
+
+	// INITIAL_OFFSET
+	data_4[0+WSX*4] = 0;
+	data_4[1+WSX*4] = 4;
+	data_4[2+WSX*4] = 6;
+	data_4[3+WSX*4] = 8;
+
+	// DYNAMIC_OFFSET
+	data_4[0+WSX*5] = 0;
+	data_4[1+WSX*5] = 6;
+	data_4[2+WSX*5] = 8;
+	data_4[3+WSX*5] = 12;
+	data_4[4+WSX*5] = 24;
+
+	// SIZE for parameter choices
+	data_4[0+WSX*6] = 9;
+	data_4[1+WSX*6] = 5;
+	data_4[2+WSX*6] = 4;
+	data_4[3+WSX*6] = 5;
+
+	// Button management
+	data_4[0+WSX*7] = 0;
+	data_4[1+WSX*7] = 0;
+	data_4[2+WSX*7] = 0;
+}
+
 
 // Fonction d'initialisation des points d'apparition des bullets
 // 
@@ -32,14 +96,27 @@ void init(uint p)
 	data_1[p] = 0;
 	data_2[p] = 0;
 	data_3[p] = 0;
+
+	if(p == 0){
+		init_pattern_parameters();
+	}
 }
 
-vec4 compute_spawn_position_angle(vec2 spawn, int nb, int nbRot, int rad, int nb_spwn)
+vec4 compute_spawn_position_angle(vec2 spawn, int pat)
 {
 	vec4 position_direction;
+	float initial_offset;
+ 	float dynamic_offset;
 
-	position_direction.x = spawn.x + cos(nb * (TWO_PI/nb_spwn) + nbRot*OFFSET_ROTATION) * rad;
-	position_direction.y = spawn.y + sin(nb * (TWO_PI/nb_spwn) + nbRot*OFFSET_ROTATION) * rad;
+ 	int nb_spwn 					= data_4[data_4[(pat*4)+WSX]+WSX*2];
+ 	int rad 							= data_4[data_4[(pat*4)+1+WSX]+WSX*(3)];
+  initial_offset 				= (data_4[data_4[(pat*4)+2+WSX]+WSX*(4)] == 0 ? 0 : PI/data_4[data_4[(pat*4)+2+WSX]+WSX*(4)]);
+ 	dynamic_offset			 	= (data_4[data_4[(pat*4)+3+WSX]+WSX*(5)] == 0 ? 0 : PI/data_4[data_4[(pat*4)+3+WSX]+WSX*(5)]);
+
+	int nb = step%nb_spwn;
+
+	position_direction.x = spawn.x + cos(nb * (TWO_PI/nb_spwn) + initial_offset + step*dynamic_offset) * rad;
+	position_direction.y = spawn.y + sin(nb * (TWO_PI/nb_spwn) + initial_offset + step*dynamic_offset) * rad;
 
 	position_direction.z = (position_direction.x - spawn.x)*1000;
 	position_direction.w = (position_direction.y - spawn.y)*1000;
@@ -124,9 +201,41 @@ void compute_global_movement(uint x, uint y, uint p)
 void render(uint x, uint y, uint p)
 {
 	if(data_1[p] == 1)
-		data_0[p] = GREEN;
+		data_0[p] = CYAN;
 	if(data_1[p] == 2)
-		data_0[p] = RED;
+		data_0[p] = PINK;
+}
+
+
+void click_manager()
+{
+	if(mouse_button == 1)
+		data_4[0+WSX*7] += 1;
+	if(mouse_button == 2)
+		data_4[1+WSX*7] += 1;
+	if(mouse_button == 4)
+		data_4[2+WSX*7] += 1;
+}
+
+void control_management()
+{
+	if(data_4[2+WSX*7] != 0){
+		data_4[2+WSX*7] = 0;
+		data_4[2] = (data_4[2]+1)%2;
+	}
+
+	if(data_4[1+WSX*7] != 0){
+		data_4[1+WSX*7] = 0;
+		int pat = data_4[2];
+		data_4[pat] = (data_4[pat]+1)%4 + (pat*4);
+	}
+	
+	if(data_4[0+WSX*7] != 0){
+		data_4[0+WSX*7] = 0;
+		int pat = data_4[2];
+		int s = data_4[(data_4[pat]%4)+WSX*6];
+		data_4[data_4[pat]+WSX] = (data_4[data_4[pat]+WSX]+1)%s;
+	}
 }
 
 
@@ -136,7 +245,7 @@ void main()
 	uint y = gl_GlobalInvocationID.y;
 	uint p = x + y * WSX;
 
-	data_0[p] = BLACK;
+	data_0[p] = GREY;
 	
 	if(step == 0){
 		// Initialisation du point d'apparition des bullets
@@ -144,8 +253,14 @@ void main()
 	}
 	else{
 		// Apparition des bullets
-		spawn_bullet(x,y,p,compute_spawn_position_angle(SPAWN1, step%NB_SPWN1, step, RAD1, NB_SPWN1), 1);
-		spawn_bullet(x,y,p,compute_spawn_position_angle(SPAWN2, step%NB_SPWN2, step, RAD2, NB_SPWN2), 2);
+		spawn_bullet(x,y,p,compute_spawn_position_angle(SPAWN1, 0), 1);
+		spawn_bullet(x,y,p,compute_spawn_position_angle(SPAWN2, 1), 2);
+
+		// Check for interactions
+		click_manager();
+		if(mouse_button == 0)
+			control_management();
+
 		// Calcul du deplacement puis affichage du rendu
 		if(step%2 == 0)
 			compute_global_movement(x,y,p);
