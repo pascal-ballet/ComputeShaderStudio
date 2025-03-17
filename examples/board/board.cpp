7170f7ce27918f2836ec7eda13e52626e94d3140
Pascal Ballet <pascal.ballet@gmail.com>
2025-03-17 08:56:22 +0100

    simplify board example

--- examples/board/board.cpp
+++ examples/board/board.cpp
@@ -1,11 +1,12 @@
+const int WIDTH = 16;
 void main() {
 
     uint x = gl_GlobalInvocationID.x;
     uint y = gl_GlobalInvocationID.y;
     uint p = x + y*WSX;
 
-	int case_x = int(floor( (x-mousex+WSX) / (150.0f*atan(3.14f*float(x+40.0f*sin(float(step)/10.0f))/float(WSX) - 3.14f ))));
-	int case_y = int(floor( (y-mousey+WSY) / (150.0f*atan(3.14f*float(y+30.0f*cos(float(step)/10.0f))/float(WSY) - 3.14f ))));
+	int case_x = int(floor( (x / WIDTH)) ); // between 0 and WSX/16 - 1
+	int case_y = int(floor( (y / WIDTH)) ); // between 0 and WSX/16 - 1
 
     if ( (case_x + case_y)%2 == 0) // Case Blanche
         data_0[p] = int(0xFFFF00FF) ; // Fond
