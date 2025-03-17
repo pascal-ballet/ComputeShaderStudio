0209049a423aadb89005ec52008edfdcc2f24811
AdrienFai2lr <adrien.fllr@gmail.com>
2025-03-17 20:41:44 +0100

    fond noir de neural pulse

---
+++ examples/neural_pulse.gd
@@ -0,0 +1,12 @@
+extends Node
+
+# Variables membres de la classe
+var my_variable = 0
+
+# Fonction appelée lorsque le nœud entre dans l'arbre de scène
+func _ready():
+	pass # Remplacez par le code d'initialisation
+
+# Fonction appelée à chaque image
+func _process(delta):
+	pass # Remplacez par votre code de mise à jour
